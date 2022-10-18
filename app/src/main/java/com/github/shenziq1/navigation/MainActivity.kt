0220401017261eee24eb19461ce2f97db226d7b7
shenziq2 <zshen331@gatech.edu>
2022-10-17 23:21:57 -0400

    update topbar

--- app/src/main/java/com/github/shenziq1/navigation/MainActivity.kt
+++ app/src/main/java/com/github/shenziq1/navigation/MainActivity.kt
@@ -3,24 +3,18 @@ package com.github.shenziq1.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key.Companion.Tab
-import androidx.compose.ui.semantics.Role.Companion.Tab
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.github.shenziq1.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,41 +36,62 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Alpha() {
-    Text(text = "Alpha")
+    Text(text = "Alpha", fontSize = 40.sp)
 }
 
 @Composable
 fun Beta() {
-    Text(text = "Beta")
+    Text(text = "Beta", fontSize = 40.sp)
 }
 
 @Composable
-fun Charlie(){
-    Text(text = "Charlie")
+fun Charlie() {
+    Text(text = "Charlie", fontSize = 40.sp)
 }
 
 @Composable
-fun App(){
+fun App() {
     val navController = rememberNavController()
     var selectedItem by remember { mutableStateOf(0) }
     val items = listOf("Songs", "Artists", "Playlists")
 //
-    Scaffold(bottomBar = {
-        BottomNavigation {
-            items.forEachIndexed { index, item ->
-                BottomNavigationItem(
-                    icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
-                    label = { Text(item) },
-                    selected = selectedItem == index,
-                    onClick = {
-                        selectedItem = index
-                        navController.navigate(item)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Simple TopAppBar") },
+                navigationIcon = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = null)
                     }
-                )
+                },
+                actions = {
+                    // RowScope here, so these icons will be placed horizontally
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+                    }
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+                    }
+                }
+            )
+        },
+
+        bottomBar = {
+            BottomNavigation {
+                items.forEachIndexed { index, item ->
+                    BottomNavigationItem(
+                        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                        label = { Text(item) },
+                        selected = selectedItem == index,
+                        onClick = {
+                            selectedItem = index
+                            navController.navigate(item)
+                        }
+                    )
+                }
             }
-        }
-    }) {
-        NavHost(navController = navController, startDestination = "Artists"){
+        }) {
+        NavHost(navController = navController, startDestination = "Songs") {
             composable(route = "Songs") {
                 Alpha()
             }
