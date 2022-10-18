0fc934ee79ee9c24ccd8a938ecc4ce163ffed9b1
shenziq2 <zshen331@gatech.edu>
2022-10-18 12:43:28 -0400

    made top bar

--- app/src/main/java/com/github/shenziq1/navigation/MainActivity.kt
+++ app/src/main/java/com/github/shenziq1/navigation/MainActivity.kt
@@ -3,17 +3,23 @@ package com.github.shenziq1.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.github.shenziq1.navigation.ui.theme.NavigationTheme
 
@@ -36,7 +42,69 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Alpha() {
-    Text(text = "Alpha", fontSize = 40.sp)
+    val alphaNaviController = rememberNavController()
+    val backStackEntry by alphaNaviController.currentBackStackEntryAsState()
+    val currentScreen = backStackEntry?.destination?.route ?: "alphaStart"
+
+    Scaffold(
+        topBar = {
+            TopAppBar() {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        onClick = { alphaNaviController.navigate("alphaStart") }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "navigation back"
+                        )
+                    }
+                    Text(text = "name")
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.MoreVert,
+                            contentDescription = "navigation back"
+                        )
+                    }
+                }
+            }
+        },
+    ) {
+        NavHost(navController = alphaNaviController, startDestination = currentScreen) {
+            composable("alphaStart") {
+                AlphaStart(onAlphaStartClicked = {
+                    alphaNaviController.navigate("alphaInner") {
+                    }
+                })
+            }
+            composable("alphaInner") {
+                AlphaInner()
+            }
+        }
+    }
+
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AlphaStart(onAlphaStartClicked: () -> Unit) {
+    Card(onClick = onAlphaStartClicked) {
+        Text(text = "alpha start")
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AlphaInner() {
+    Card(onClick = {}) {
+        Text(text = "inside alpha")
+    }
 }
 
 @Composable
@@ -56,26 +124,6 @@ fun App() {
     val items = listOf("Songs", "Artists", "Playlists")
 //
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Simple TopAppBar") },
-                navigationIcon = {
-                    IconButton(onClick = { /* doSomething() */ }) {
-                        Icon(Icons.Filled.Menu, contentDescription = null)
-                    }
-                },
-                actions = {
-                    // RowScope here, so these icons will be placed horizontally
-                    IconButton(onClick = { /* doSomething() */ }) {
-                        Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
-                    }
-                    IconButton(onClick = { /* doSomething() */ }) {
-                        Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
-                    }
-                }
-            )
-        },
-
         bottomBar = {
             BottomNavigation {
                 items.forEachIndexed { index, item ->
